22a431a0fc9dd8da03d7899bea249a0e86ed0ea4
thereallucas98 <lucas1998david@gmail.com>
2022-06-26 09:36:12 -0300

    Create User - should be able to create a new user

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("Create a user", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "David Lucas",
+      email: "lucas@test.com",
+      password: "123456",
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+});
