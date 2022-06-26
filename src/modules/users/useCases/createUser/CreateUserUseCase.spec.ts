4ec162a67d98b2e31face533727fcd80d8e39440
thereallucas98 <lucas1998david@gmail.com>
2022-06-26 09:39:09 -0300

    Create User - should not be able to create a user with an existent email

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let createUserUseCase: CreateUserUseCase;
@@ -18,5 +19,21 @@ describe("Create a user", () => {
     });
 
     expect(user).toHaveProperty("id");
+  });
+
+  it("should not be able to create a user with an existent email", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "David Lucas",
+        email: "lucas@test.com",
+        password: "1234",
+      });
+
+      await createUserUseCase.execute({
+        name: "Lucas Santos",
+        email: "lucas@test.com",
+        password: "4321",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
   });
 });
